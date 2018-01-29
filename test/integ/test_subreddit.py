4281cc77d6ea627b16baf0a34321b1361450e344
Dan Urson <dan@plygrnd.net>
2018-01-28 23:11:22 -0500

    GODDAMMIT!

--- test/integ/test_subreddit.py
+++ test/integ/test_subreddit.py
@@ -5,12 +5,6 @@ import subreddit
 s = subreddit.Stats(aws_profile='tinkerbell')
 
 
-<<<<<<< HEAD
 def test_fetch_reddit_account():
     u = s.fetch_reddit_account('beefythecat')
     assert u
-=======
-def test_is_banned():
-    u = s.is_banned('beefythecat')
-    assert u is False
->>>>>>> 7923c2d3ca38d1d6920d44c702d6e6e187d22a36
