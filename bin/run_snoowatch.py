ec91199989eeee1fc8017d7bc01e8f50a1924303
Dan Urson <durson@amazon.com>
2020-08-16 21:39:33 -0400

    Implement arguments within run_snoowatch
    Been noodling on the unusability of the runtime script - it
    didn't have the ability to accept the inputs we needed it to expect.
    So, I took the easy/lazy way out and used argparse to define the
    SM secret/region/subreddit we want.
    
    Of course, this broke everything on which this thing depends,
    because the Elasticsearch class is broken as hell. As such,
    as of this commit, everything useful is commented out. But...
    this is a good starting point.

--- bin/run_snoowatch.py
+++ bin/run_snoowatch.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
 
+import argparse
+
 from datetime import datetime
 from sys import argv
 
-from reddit import Subreddit
-from search import ESClient
+from snoowatch.log import log_generator
+from snoowatch.reddit import Subreddit
+from snoowatch.search import ESClient
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-k", "--reddit-api-secret", help="AWS Secrets Manager Secret")
+parser.add_argument("-r", "--aws-region", help="AWS Region")
+parser.add_argument("-s", "--subreddit", help="Subreddit to watch")
+args = parser.parse_args()
 
 
 class Watcher(object):
     def __init__(self, subreddit):
-        self.reddit = Subreddit(reddit_api_secret_name, aws_region, subreddit=None)
+        subreddit = args.subreddit if args.subreddit else None
+        self.reddit = Subreddit(args.reddit_api_secret, args.aws_region)
+        """
         self.es = ESClient(
             aws_profile="snoowatch", sub="anxiety", cluster_url="172.18.0.1"
         )
@@ -19,10 +31,14 @@ class Watcher(object):
         self.last_post = datetime.strftime(
             datetime.strptime(self.results[0]["created"], "%Y-%m-%d %H:%M:%S").date(),
             "%Y/%m/%d",
-        )
+        )"""
 
-        self.logger = logger(__name__)
+        self.logger = log_generator(__name__)
 
+        test_redditor = "beefythecat"
+        r = self.reddit.get_redditor(test_redditor)
+
+    """
     def watch_subreddit(self):
 
         self.logger.info("Time of last post in index: {}".format(self.last_post))
@@ -37,10 +53,14 @@ class Watcher(object):
 
         self.logger.info("Index updated. Continuing to stream new submissions.")
         self.es.stream_submissions()
+        """
 
 
 if __name__ == "__main__":
-    logger = log_generator()
-    subreddit = Watcher(argv[1])
+    logger = log_generator(__name__)
+    subreddit = args.subreddit if args.subreddit else None
+    subreddit = Watcher(subreddit)
+    """
     logger.info(f"Starting to monitor r/{subreddit}")
     subreddit.watch_subreddit()
+    """
